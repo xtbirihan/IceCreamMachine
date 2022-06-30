ca6b0f1e9a2a13cac3e1b33a00a3cd7ecc1108ec
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-30 18:29:23 +0000

    Step 1: Initial Application

---
+++ webapp/controller/Startpage.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.01.controller.Startpage", {
+		onInit: function () {}
+	});
+});
+
