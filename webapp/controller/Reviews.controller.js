1345470f2a28f52f93d31c602e8272de6a506b41
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-07-01 12:11:10 +0000

    Step 12: Timeline

--- webapp/controller/Reviews.controller.js
+++ webapp/controller/Reviews.controller.js
@@ -1,9 +1,46 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
-	"use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/format/DateFormat"
+], function (Controller, DateFormat) {
+    "use strict";
 
-	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.controller.Reviews", {
+    return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.controller.Reviews", {
+        onInit: function () {
+            this.oTimeLine = this.byId("timeline");
+        },
 
-	});
+        addReview: function () {
+            var oModel = this.getView().getModel("reviews"),
+                oData = oModel.getData(),
+                oTemplateEntry = oData.UserReviews[0];
+            oTemplateEntry.template = false;
+            oData.UserReviews.unshift({
+                "user": "",
+                "userPic": "",
+                "rating": 10,
+                "quote": "",
+                "dateTime": "now",
+                "template": true
+            });
+            oModel.setData(oData);
+
+        },
+
+        formatDateTime: function (dateTime) {
+            var oDateInstance = DateFormat.getDateInstance();
+            return oDateInstance.format(oDateInstance.parse(dateTime));
+        },
+        onNavButtonPressed: function () {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("home");
+
+        },
+        onHorizontalSwitchChange: function (event) {
+            if (event.getParameter("state")) {
+                this.oTimeline.setAxisOrientation("Horizontal");
+            } else {
+                this.oTimeline.setAxisOrientation("Vertical");
+            }
+        }
+    });
 });
