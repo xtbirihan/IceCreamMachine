1e288465fc56e2ad882a25da4e50c226d8866099
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-07-01 09:53:27 +0000

    Step 7: Header Container and Radial Micro Chart

--- webapp/controller/ProcessFlow.controller.js
+++ webapp/controller/ProcessFlow.controller.js
@@ -1,9 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+    "sap/base/strings/formatMessage"
+], function(Controller, formatMessage) {
 	"use strict";
 
 	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.controller.ProcessFlow", {
+		formatMessage: formatMessage,
 
+		onNavButtonPressed: function () {
+			this.getOwnerComponent().getRouter().navTo("home");
+		}
 	});
 });
