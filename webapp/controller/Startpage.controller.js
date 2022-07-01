9f023c8d388589f20e7c4f5c52fbdf1c609f43e7
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-07-01 08:25:53 +0000

    Step 3: Launch Tile and Slide Tile

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -1,12 +1,17 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
     "sap/ui/core/format/NumberFormat",
     "sap/base/strings/formatMessage"
-], function (Controller, NumberFormat, formatMessage) {
+], function (Controller, JSONModel, NumberFormat, formatMessage) {
     "use strict";
 
-    return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.01.controller.Startpage", {
-        onInit: function () { },
+    return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.controller.Startpage", {
+        onInit: function () {
+            var sDataPath = sap.ui.require.toUrl("sap/suite/ui/commons/demokit/tutorial/icecream/model/data") + "/News.json";
+            var oModel = new JSONModel(sDataPath);
+            this.getView().setModel(oModel, "news");
+        },
 
         formatMessage: formatMessage,
 
@@ -21,16 +26,21 @@ sap.ui.define([
             var fPercent = (iSum / aNodes.length) * 100;
             return fPercent.toFixed(0);
         },
-        getEntityCount: function(entities) { 
+        getEntityCount: function (entities) {
             return entities && entities.length || 0;
         },
-        formatNumber: function(value) { 
+        formatNumber: function (value) {
             var oFloatFormatter = NumberFormat.getFloatInstance({
                 style: "short",
                 decimals: 1
             });
             return oFloatFormatter.format(value);
+        },
+        formatJSONDate: function (date) {
+            var oDate = new Date(Date.parse(date));
+            return oDate.toLocaleDateString();
         }
+
     });
 });
 
